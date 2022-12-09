b88b575a927b956ac47084bb3975f83afbf4cb42
roldanws <eumsystems@gmail.com>
2022-12-09 14:13:56 -0600

    Delete walmart/app directory

--- walmart/app/Monitor_02/__init__.py
+++
@@ -1,3 +0,0 @@
-# __init__.py
-
-__all__ = ["Botones", "PuertoDeComunicacion", "obtenerNombreDelPuerto"]
--- walmart/app/Variables/Borrador/Modificar__init__.py
+++
@@ -1,6 +0,0 @@
-# __init__.py
-from .Variable import Variable
-from .InterfazGrafica import InterfazGrafica
-
-
-__all__ = ["Variable", "InterfazGrafica"]
